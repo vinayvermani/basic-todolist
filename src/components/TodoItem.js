2b71d0bc28d8250f8aa42747c2df24820966055f
Payal Vermani <payalvermani@Payals-MacBook-Pro.local>
2018-11-06 20:50:25 -0500

    webpack added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-// class TodoItem extends React.Component {
-//     render() {
-//         return (
-//             <h1>vermani</h1>
-//         )
-//     }
-// }
-
 
 function TodoItem(props) {
-    return  <h1>{props.message}</h1>
+    return <h1>{props.message}</h1>
 }
-module.exports.TodoItem = TodoItem;
+
+export {
+    TodoItem
+}
